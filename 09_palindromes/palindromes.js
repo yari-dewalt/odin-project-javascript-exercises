2a02c0db21a8634a2d96b2d44f37cdd4546d2197
yari-dewalt <yari.dewalt@gmail.com>
2023-05-30 18:39:34 -0700

    Finish palindrome exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,20 @@
-const palindromes = function () {
-
-};
+const palindromes = function (arr) {
+    let palindrome = false;
+    arr = arr.replace(/[.,\/#!$%\^&\*;:{}=\-_`~() ]/g, "");
+    arr = arr.toLowerCase();
+    console.log(arr);
+  
+      for (let i = 0; i < arr.length; i++) {
+          if (arr[i] === arr[arr.length - (i + 1)]) {
+              palindrome = true;
+          }
+          else {
+              palindrome = false;
+              return palindrome;
+          }
+      }
+     return palindrome;
+  };
 
 // Do not edit below this line
 module.exports = palindromes;
