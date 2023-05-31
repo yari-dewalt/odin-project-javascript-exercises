5e57b3aca6116959c02aeaa670951710379addf8
yari-dewalt <yari.dewalt@gmail.com>
2023-05-30 18:52:21 -0700

    Complete fibonacci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
+const fibonacci = function(member) {
+    let arr = [1, 1];
 
+    if (member < 0) {
+      return "OOPS";
+    }
+    for (let i = 2; i < member; i++) {
+        arr[i] = arr[i-1] + arr[i-2];
+    }
+
+    return(arr[member - 1]);
 };
 
 // Do not edit below this line
